cba075ca99e2fd7c453563272ada1fbd6ed17129
Rafael M V <rafaelmendoncavaz@hotmail.com>
2024-02-20 16:45:54 -0300

    Atividade concluída

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    if(player1 === player2) {
+        return "Empate!"
+    }
+    else if((player1 === "Pedra" && player2 === "Tesoura")
+    || (player1 === "Papel" && player2 === "Pedra")
+    || (player1 === "Tesoura" && player2 === "Papel")) {
+        return "Jogador 1 venceu!"
+    }
+    else {
+        return "Jogador 2 venceu!"
+    }
+}
